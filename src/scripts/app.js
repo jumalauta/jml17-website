46604209c53e67477a469e000a6ffa8a3816d5a9
Teemu Lahti <teemu.lahti@gmail.com>
2017-01-05 19:37:27 +0200

    basic website structure for styles, scripts and added navbar & hero element

---
+++ src/scripts/app.js
@@ -0,0 +1,41 @@
+import Parallax from './parallax';
+
+class App {
+  constructor() {
+    console.log('Jumalauta 17, September 1st - 3rd, 2017');
+
+    this.html = document.getElementsByTagName('html')[0];
+    this.body = document.getElementsByTagName('body')[0];
+
+    // bind methods
+    this.handleScroll = this.handleScroll.bind(this);
+
+    // init parallax elements
+    this.parallaxElements = window.document.querySelectorAll('[data-parallax]');
+    this.parallax = [];
+    this.parallaxElements.forEach((element) => {
+      this.parallax.push(new Parallax(element));
+    });
+
+    window.addEventListener('scroll', this.handleScroll);
+    this.handleScroll();
+
+    // set html class name to loaded
+    this.html.className = this.html.className.replace('app-loading', 'app-loaded');
+  }
+
+  /**
+   * Update body class on window scroll event
+   */
+  handleScroll() {
+    if (window.scrollY <= 0 && this.body.className.indexOf('scroll-top') < 0) {
+      this.body.className += ' scroll-top';
+    } else if (window.scrollY > 0 && this.body.className.indexOf('scroll-top') >= 0) {
+      this.body.className = this.body.className.replace('scroll-top', '');
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  window.app = new App();
+});
---
+++ src/scripts/parallax.js
@@ -0,0 +1,74 @@
+/* global Modernizr,$ */
+
+export default class Parallax {
+  constructor(element) {
+    this.element = element;
+    this.item = element.querySelector('[data-parallax-item]');
+    this.element.className += ' parallax-container';
+    this.anchor = element.dataset.parallaxAnchor || 'center';
+    this.amount = parseInt(element.dataset.parallaxAmount || 0, 10);
+    this.offset = parseInt(element.dataset.parallaxOffset || 0, 10);
+    this.autoSize = (element.dataset.parallaxAutosize || true) === true;
+    this.unit = element.dataset.parallaxUnit || 'px';
+    this.breakpoint = element.dataset.parallaxBreakpoint;
+
+    // set initial transform
+    if (Modernizr.csstransforms3d) {
+      this.item.style.transform = 'translate3d(0, 0, 0)';
+    } else {
+      this.item.style.transform = 'translateY(0)';
+    }
+
+    window.addEventListener('scroll', this.render.bind(this));
+    this.render();
+  }
+
+  render() {
+    // get window size
+    const bodyRect = document.getElementsByTagName('body')[0].getBoundingClientRect();
+    if (bodyRect.width < this.breakpoint) {
+      this.item.style.transform = 'none';
+      return;
+    }
+
+    // get container size & position
+    const containerRect = this.element.getBoundingClientRect();
+
+    if (this.autoSize) {
+      // set parallax item height if autosize is set
+      if (this.unit === '%') {
+        this.item.style.height =
+          `${containerRect.height + (containerRect.height * ((this.amount * 2) / 100))}px`;
+      } else {
+        this.item.style.height =
+          `calc(${containerRect.height}px + ${this.amount * 2}${this.unit})`;
+      }
+    }
+
+    // check that element is visible
+    if (
+      (containerRect.top >= 0 && containerRect.top <= bodyRect.height) ||
+      (containerRect.bottom >= 0 && containerRect.bottom <= bodyRect.height)
+    ) {
+      let ratio = 0;
+      if (this.anchor === 'middle') {
+        const containerMiddle = containerRect.top + (containerRect.height / 2);
+        const screenMiddle = bodyRect.height / 2;
+        const middleDiff = screenMiddle - containerMiddle;
+        const max = containerRect.height;
+        const min = -max;
+        ratio = (middleDiff - min) / (max - min);
+      } else if (this.anchor === 'top') {
+        const diff = containerRect.height - Math.abs(containerRect.top);
+        ratio = 1 - (diff / containerRect.height);
+      }
+      const transform = ratio * this.amount;
+
+      if (Modernizr.csstransforms3d) {
+        this.item.style.transform = `translate3d(0, ${transform}${this.unit}, 0)`;
+      } else {
+        this.item.style.transform = `translateY(${transform}${this.unit})`;
+      }
+    }
+  }
+}
